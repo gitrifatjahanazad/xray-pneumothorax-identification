adeb2d980049214dcf9a7210041ea9d97d18b67f
nathshawon36 <nathshawon36@gmail.com>
2019-10-29 20:20:25 +0600

    updating.....

--- src/App.js
+++ src/App.js
@@ -97,56 +97,99 @@ function App() {
             <div class="col-sm-8">
               <h1 class="head_title">Upload X-Ray To Find Pneumothorax</h1>
             </div>
-
+           
           </div>
         </div>
       </header>
 
-      <div class="container">
-        <div className="imageSection">
+{/*main*/}
+      <div class="main_bar">
+        <div className="imageSection_main">
 
-          <h1 class="sub_title">What is pneumothorax(Collapsed lung)?</h1>
+
+          <p class="sub_title">What is pneumothorax(Collapsed lung)?</p>
           <h6 class="sub_title1">"air on the wrong side of the lung", can result in death</h6>
 
-          <div class="col-sm-4 section_1">
-            <p class="">A pneumothorax is an abnormal collection of air in the pleural space between the lung and the chest wall. Symptoms typically include sudden onset of sharp, one-sided chest pain and shortness of breath.
-            
-            Pneumothorax can be caused by a blunt chest injury, damage from underlying lung disease, or most horrifying—it may occur for no obvious reason at all. On some occasions, a collapsed lung can be a life-threatening event.
-            Pneumothorax is usually diagnosed by a radiologist on a chest x-ray, and can sometimes be very difficult to confirm. An accurate AI algorithm to detect pneumothorax would be useful in a lot of clinical scenarios.
-AI could be used to triage chest radiographs for priority interpretation, or to provide a more confident diagnosis for non-radiologists.</p>
-            <p>Check More On Wikipedia</p>
-          </div>
-
-
-          <div class="col-sm-2">
-            <div className="originalImgSec">
-              <img id="originalImg" alt="original" />
-              <label>Original Image</label>
+          <div class="row">
+          <div class="col-sm-3 section_1">
+            <p class="text-field">
+              <div>
+              <span class="text">A pneumothorax is an abnormal collection of air in the pleural space between the lung and the chest wall. Symptoms typically include sudden onset of sharp, one-sided chest pain and shortness of breath.</span>
             </div>
-</div>
-<div class="col-sm-2">
-            <div className="resultImgSec">
+            <br></br>
+            <div>
+           <span class="text">Pneumothorax can be caused by a blunt chest injury, damage from underlying lung disease, or most horrifying—it may occur for no obvious reason at all. On some occasions, a collapsed lung can be a life-threatening event.</span> 
+           </div>
+           <br></br>
+           <span class="text">Pneumothorax is usually diagnosed by a radiologist on a chest x-ray, and can sometimes be very difficult to confirm. An accurate AI algorithm to detect pneumothorax would be useful in a lot of clinical scenarios.
+AI could be used to triage chest radiographs for priority interpretation, or to provide a more confident diagnosis for non-radiologists.</span>
+</p>
+            <p class="text-field1"><span class="text_style">Check More On Wikipedia</span></p>
+          </div>
+         
+<div class="image col-sm-6">
+             
+            <div className="Image_Section">
+              <img id="originalImg" alt="Original Image" src="x-ray.png"/>
+              <label> </label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
               <img id="resultImg" alt="result" />
               <img className="loader" alt="result" src="/loading1.gif" />
-              <label>Pneumothorax Identified Region</label>
+              <label></label>                         
             </div>
-         
-            </div>
+            <p class="text_area">Pneumothorax X-rays</p>
 
-<div class="col-sm-2">hello</div>
-
-          </div>
-
-        
-
-
-
-        <div className="fileUploadBtn">Upload Your X-Ray
+            <div className="fileUploadBtn">Upload Your X-Ray
           <input id="uploadInput" type="file" name="file" onChange={onChangeHandler} accept="image/x-png" />
         </div>
 
+</div>           
+<br></br>
+<div class="col-sm-3 footer_area">
+<p class="text-field">
+          <div class="heading_sec">
+            About This Project
+            </div>
+            <br></br>
+            <span class="text">
+            &nbsp;&nbsp;&nbsp;&nbsp;This Project was developed 
+              <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;to Research Purpose of 
+              <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;Brain Station 23.
+              <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;A brain Child of kaggle 
+              <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;"SIIM-ACR Pneumothorax 
+              <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;Segmentation" 
+              <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;Used Unet with 
+              <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;inception resnet v2.
+          </span>
+        </p>
+  
+        <p class="text-field">
+            <span class="heading_sec">Special Thanks To
+              <br></br>
+             </span>
+             <span>
+             &nbsp;&nbsp;&nbsp;&nbsp;Raisul Kabir 
+              <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;Jesper
+              <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;Siddhartha
+              <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;Ekhtiar Syed
+              <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;Rishabh Agrahari
+             
+            </span>
+          </p>
 
 
+</div>
+</div>
+<hr></hr>
+
+<div class="row" id="footer">
+
+          <p><img src="kaggle.png" alt="logo"></img>
+          <img src="" alt="Brain Station 23"></img>
+          <img src="" alt="BUET"></img>
+          </p>
+          </div>
+</div>
       </div>
     </div>
   );
